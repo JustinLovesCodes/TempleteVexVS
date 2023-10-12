9c625c02873edf85aac868d88107c037a4aa475c
JustinLovesCodes <143911508+JustinLovesCodes@users.noreply.github.com>
2023-10-11 22:57:09 -0400

    robot-config.h

---
+++ robot-config.h
@@ -0,0 +1,23 @@
+using namespace vex;
+
+extern brain Brain;
+
+/*
+TO ADD A DEVICE:
+
+extern DEVICETYPE NAME;
+
+DEVICENAME = the device it is
+Ex:
+motor, limit, digital_out, rotation, ext...
+
+NAME = the name given to the device
+
+Ex:
+extern motor Motor1;
+extern rotation Gryo;
+extern digital_out Pneumatics;
+*/
+
+
+void  vexcodeInit( void );
